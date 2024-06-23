33c21d7002ad03540bebd0ba6615790387f29fb5
Eryc Jacinto <121405251+ErycMJ@users.noreply.github.com>
2024-06-23 17:39:06 -0300

    Update drop_trigger_proc.sql

--- scripts_sql/drop_trigger_proc.sql
+++ scripts_sql/drop_trigger_proc.sql
@@ -1,85 +1,25 @@
--- FUNCION:
-CREATE FUNCTION VerificarConflitoHorario (@DentistaID INT, @DataHoraInicio DATETIME, @DataHoraFim DATETIME)
-RETURNS BIT
+CREATE FUNCTION FormatarDataHoraBrasileira(@datahora datetime)
+RETURNS NVARCHAR(100)
 AS
 BEGIN
-DECLARE @ExisteConflito BIT;
+    DECLARE @data_formatada NVARCHAR(100);
 
-IF EXISTS (
-    SELECT 1
-    FROM Agendas
-    WHERE DentistaID = @DentistaID
-    AND DataHoraInicio < @DataHoraFim
-    AND DataHoraFim > @DataHoraInicio
-    AND Disponivel = 0  -- Considera apenas tratamentos marcados (não disponíveis)
-)
-BEGIN
-    SET @ExisteConflito = 1;  -- Há conflito de horário
-END
-ELSE
-BEGIN
-    SET @ExisteConflito = 0;  -- Não há conflito de horário
-END
+    SELECT @data_formatada = CONVERT(NVARCHAR, DATEPART(day, @datahora)) + ' de ' + 
+                             CASE DATEPART(month, @datahora)
+                                 WHEN 1 THEN 'janeiro' WHEN 2 THEN 'fevereiro' WHEN 3 THEN 'março' 
+                                 WHEN 4 THEN 'abril' WHEN 5 THEN 'maio' WHEN 6 THEN 'junho' 
+                                 WHEN 7 THEN 'julho' WHEN 8 THEN 'agosto' WHEN 9 THEN 'setembro' 
+                                 WHEN 10 THEN 'outubro' WHEN 11 THEN 'novembro' WHEN 12 THEN 'dezembro'
+                             END + ' de ' + 
+                             CONVERT(NVARCHAR, DATEPART(year, @datahora)) + ' às ' + 
+                             CONVERT(NVARCHAR, DATEPART(hour, @datahora)) + ':' + 
+                             RIGHT('0' + CONVERT(NVARCHAR, DATEPART(minute, @datahora)), 2);
 
-RETURN @ExisteConflito;
+    RETURN @data_formatada;
+END;
+GO
 
-
-
--- PROCEDURE:
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TRIGGER:
---- Ideia para Trigger (teste)
--- A cada geração de consulta o valor de receita é atualizado no insert 
-
--- CREATE TRIGGER atualizar_valor_receita
--- AFTER INSERT ON Consultas
--- FOR EACH ROW
--- BEGIN
---     DECLARE novo_valor DECIMAL(10, 2);
-
---     SELECT SUM(r.Valor) INTO novo_valor
---     FROM Receitas r
---     WHERE r.PacienteID = NEW.PacienteID;
-
---     UPDATE Receitas
---     SET Valor = novo_valor
---     WHERE PacienteID = NEW.PacienteID;
--- END;
-
--- OU
-
--- CREATE TRIGGER trg_AfterInsertConsulta_UpdateAgenda
--- ON Consultas
--- AFTER INSERT
--- AS
--- BEGIN
--- -- Declaração de variáveis
--- DECLARE @DentistaID INT, @datahora DATETIME;
-
--- -- Selecionar os dados da nova consulta inserida
--- SELECT @DentistaID = i.DentistaID, 
---        @DataHora = i.DataHora
--- FROM Inserted i;
-
--- -- Atualizar a disponibilidade do dentista para FALSE
--- UPDATE Agendas
--- SET Disponivel = 0
--- WHERE DentistaID = @DentistaID AND DataHora = @DataHora;
--- END;
-
--- Criação de um trigger para a tabela Consultas que automaticamente insere um registro na tabela Agendas quando uma nova consulta é marcada
+-- Trigger para inserir automaticamente na tabela Agendas
 CREATE TRIGGER trg_AfterInsertConsultas
 ON Consultas
 AFTER INSERT
@@ -95,18 +35,62 @@ BEGIN
 END;
 GO
 
-SELECT * FROM Agendas
-SELECT * FROM Consultas
+-- Procedimento armazenado para excluir consulta
+CREATE PROCEDURE ExcluirConsulta
+    @ConsultaID INT
+AS
+BEGIN
+    DELETE FROM Consultas
+    WHERE ConsultaID = @ConsultaID;
+END;
+GO
 
-INSERT INTO Consultas (PacienteID, DentistaID, DataHora, Motivo) VALUES
-(5, 1, '2024-07-12 09:00:00', 'Teste')
+CREATE FUNCTION ConsultasComNomes()
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT 
+        C.ConsultaID,
+        C.PacienteID,
+        P.Nome AS NomePaciente,
+        C.DentistaID,
+        D.Nome AS NomeDentista,
+        dbo.FormatarDataHoraBrasileira(C.DataHora) AS DataHoraFormatada,
+        C.Motivo
+    FROM Consultas C
+    INNER JOIN Pacientes P ON C.PacienteID = P.PacienteID
+    INNER JOIN Dentistas D ON C.DentistaID = D.DentistaID
+);
+GO
 
+CREATE PROCEDURE InserirConsulta
+    @NomePaciente VARCHAR(100),
+    @NomeDentista VARCHAR(100),
+    @DataHora DATETIME,
+    @Motivo VARCHAR(255)
+AS
+BEGIN
+    DECLARE @PacienteID INT;
+    DECLARE @DentistaID INT;
 
+    -- Obter PacienteID pelo Nome do Paciente
+    SELECT @PacienteID = PacienteID
+    FROM Pacientes
+    WHERE Nome = @NomePaciente;
 
+    -- Obter DentistaID pelo Nome do Dentista
+    SELECT @DentistaID = DentistaID
+    FROM Dentistas
+    WHERE Nome = @NomeDentista;
 
+    -- Inserir a consulta com os IDs obtidos
+    INSERT INTO Consultas (PacienteID, DentistaID, DataHora, Motivo)
+    VALUES (@PacienteID, @DentistaID, @DataHora, @Motivo);
 
-
-
-
-
+    -- Retornar os dados da consulta recém-inserida com nomes
+    SELECT *
+    FROM ConsultasComNomes()
+    WHERE ConsultaID = SCOPE_IDENTITY(); -- SCOPE_IDENTITY() retorna o ID da consulta inserida
+END;
 
