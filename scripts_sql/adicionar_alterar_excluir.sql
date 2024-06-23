c64cd6696f34fb2064948d388bb72e17bec4a1c2
Alexandre Destro <aalexandredestro@gmail.com>
2024-06-23 20:58:23 -0300

    add, updt, del

---
+++ scripts_sql/adicionar_alterar_excluir.sql
@@ -0,0 +1,15 @@
+-- add consulta
+INSERT INTO Consultas (PacienteID, DentistaID, DataHora, Motivo) VALUES
+(20, 1, '2024-12-01 13:30:00', 'Consulta de rotina');
+GO
+
+-- updt
+UPDATE Consultas
+SET DataHora = '2024-12-04 13:30:00'
+WHERE PacienteID = 20;
+GO
+
+-- del
+DELETE FROM Consultas
+WHERE PacienteID = 20;
+GO
