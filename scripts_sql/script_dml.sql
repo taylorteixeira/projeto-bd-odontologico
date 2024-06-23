4a2e2457e88e14e2d9956a5cf6bfa7ee1b64499f
Eryc Jacinto <121405251+ErycMJ@users.noreply.github.com>
2024-06-23 17:42:30 -0300

    Update script_dml.sql

--- scripts_sql/script_dml.sql
+++ scripts_sql/script_dml.sql
@@ -1,5 +1,4 @@
--- INSERT INTOSSS
-
+-- Inserção de dados nas tabelas
 INSERT INTO Pacientes (Nome, DataNascimento, Sexo, Endereco, Telefone, Email) VALUES
 ('João Silva', '1985-06-15', 'M', 'Rua das Flores, 123', '11987654321', 'joao.silva@gmail.com'),
 ('Maria Oliveira', '1990-04-25', 'F', 'Av. Paulista, 456', '11976543210', 'maria.oliveira@gmail.com'),
@@ -37,17 +36,17 @@ INSERT INTO Consultas (PacienteID, DentistaID, DataHora, Motivo) VALUES
 (10, 2, '2024-06-14 10:00:00', 'Dor de dente');
 GO
 
-INSERT INTO Tratamentos (Descricao, Custo) VALUES
-('Limpeza Dentária', 200.00),
-('Canal Radicular', 800.00),
-('Extração de Dente', 300.00),
-('Clareamento Dental', 1000.00),
-('Implante Dentário', 3000.00),
-('Prótese Dentária', 2500.00),
-('Restauração Dental', 500.00),
-('Tratamento de Gengivite', 600.00),
-('Ortodontia (Aparelho)', 3500.00),
-('Odontopediatria (Consulta)', 150.00);
+INSERT INTO Tratamentos (Descricao, Custo, DuracaoMinutos) VALUES
+('Limpeza Dentária', 200.00, 30),
+('Canal Radicular', 800.00, 60),
+('Extração de Dente', 300.00, 180),
+('Clareamento Dental', 1000.00, 40),
+('Implante Dentário', 3000.00, 60),
+('Prótese Dentária', 2500.00, 50),
+('Restauração Dental', 500.00, 20),
+('Tratamento de Gengivite', 600.00, 30),
+('Ortodontia (Aparelho)', 3500.00, 1000),
+('Odontopediatria (Consulta)', 150.00, 180);
 GO
 
 INSERT INTO Prontuarios (PacienteID, DentistaID, Data, Diagnostico, TratamentoID, Observacoes) VALUES
@@ -55,10 +54,10 @@ INSERT INTO Prontuarios (PacienteID, DentistaID, Data, Diagnostico, TratamentoID
 (2, 2, '2024-06-12 10:00:00', 'Cárie dentária', 2, 'Recomendada realização de canal.'),
 (3, 3, '2024-06-12 11:00:00', 'Gengivite', 3, 'Necessária extração de dente.'),
 (4, 4, '2024-06-12 14:00:00', 'Boa saúde bucal', 1, 'Paciente em boas condições.'),
-(5, 5, '2024-06-12 15:00:00', 'Falta de dente', 4, 'Implante dentário recomendado.'),
-(6, 6, '2024-06-13 09:30:00', 'Perda de dente', 5, 'Necessidade de prótese dentária.'),
-(7, 7, '2024-06-13 10:30:00', 'Cárie profunda', 6, 'Restauração necessária.'),
-(8, 8, '2024-06-13 11:30:00', 'Lesão benigna', 7, 'Tratamento de lesão realizado.'),
+(5, 5, '2024-06-12 15:00:00', 'Perda de dente', 5, 'Recomendada colocação de implante.'),
+(6, 6, '2024-06-13 09:30:00', 'Dente quebrado', 6, 'Necessária colocação de prótese.'),
+(7, 7, '2024-06-13 10:30:00', 'Cárie dentária', 7, 'Recomendada restauração.'),
+(8, 8, '2024-06-13 11:30:00', 'Lesão na boca', 8, 'Tratamento necessário para gengivite.'),
 (9, 1, '2024-06-14 09:00:00', 'Boa saúde bucal', 1, 'Paciente em boas condições.'),
 (10, 2, '2024-06-14 10:00:00', 'Cárie dentária', 2, 'Recomendada realização de canal.');
 GO
@@ -68,36 +67,36 @@ INSERT INTO Pagamentos (PacienteID, Valor, DataPagamento, MetodoPagamento) VALUE
 (2, 800.00, '2024-06-12', 'Dinheiro'),
 (3, 300.00, '2024-06-12', 'Cartão de Débito'),
 (4, 200.00, '2024-06-12', 'Cartão de Crédito'),
-(5, 1000.00, '2024-06-12', 'Boleto Bancário'),
-(6, 2500.00, '2024-06-13', 'Transferência Bancária'),
-(7, 500.00, '2024-06-13', 'Cartão de Crédito'),
+(5, 3000.00, '2024-06-12', 'Transferência Bancária'),
+(6, 2500.00, '2024-06-13', 'Cartão de Crédito'),
+(7, 500.00, '2024-06-13', 'Cartão de Débito'),
 (8, 600.00, '2024-06-13', 'Dinheiro'),
 (9, 200.00, '2024-06-14', 'Cartão de Crédito'),
 (10, 800.00, '2024-06-14', 'Dinheiro');
 GO
 
 INSERT INTO Agendas (PacienteID, DentistaID, DataHoraInicio, DataHoraFim, Disponivel) VALUES
-(1, 1, '2024-06-12 08:00:00', '2024-06-12 09:00:00', 0),
-(2, 2, '2024-06-12 09:00:00', '2024-06-12 10:00:00', 0),
-(3, 3, '2024-06-12 10:00:00', '2024-06-12 11:00:00', 0),
-(4, 4, '2024-06-12 11:00:00', '2024-06-12 12:00:00', 0),
-(5, 5, '2024-06-12 13:00:00', '2024-06-12 14:00:00', 0),
-(6, 6, '2024-06-12 14:00:00', '2024-06-12 15:00:00', 0),
-(7, 7, '2024-06-12 15:00:00', '2024-06-12 16:00:00', 0),
-(8, 8, '2024-06-12 16:00:00', '2024-06-12 17:00:00', 0),
-(9, 1, '2024-06-13 08:00:00', '2024-06-13 09:00:00', 0),
-(10, 2, '2024-06-13 09:00:00', '2024-06-13 10:00:00', 0);
+(1, 1, '2024-06-12 09:00:00', '2024-06-12 09:30:00', 0),
+(2, 2, '2024-06-12 10:00:00', '2024-06-12 10:30:00', 0),
+(3, 3, '2024-06-12 11:00:00', '2024-06-12 11:30:00', 0),
+(4, 4, '2024-06-12 14:00:00', '2024-06-12 14:30:00', 0),
+(5, 5, '2024-06-12 15:00:00', '2024-06-12 15:30:00', 0),
+(6, 6, '2024-06-13 09:30:00', '2024-06-13 10:00:00', 0),
+(7, 7, '2024-06-13 10:30:00', '2024-06-13 11:00:00', 0),
+(8, 8, '2024-06-13 11:30:00', '2024-06-13 12:00:00', 0),
+(9, 1, '2024-06-14 09:00:00', '2024-06-14 09:30:00', 0),
+(10, 2, '2024-06-14 10:00:00', '2024-06-14 10:30:00', 0);
 GO
 
 INSERT INTO Receitas (PacienteID, Valor, DataReceita, Descricao) VALUES
-(1, 200.00, '2024-06-12', 'Pagamento por limpeza dentária.'),
-(2, 800.00, '2024-06-12', 'Pagamento por canal radicular.'),
-(3, 300.00, '2024-06-12', 'Pagamento por extração de dente.'),
-(4, 200.00, '2024-06-12', 'Pagamento por limpeza dentária.'),
-(5, 1000.00, '2024-06-12', 'Pagamento por clareamento dental.'),
-(6, 2500.00, '2024-06-13', 'Pagamento por prótese dentária.'),
-(7, 500.00, '2024-06-13', 'Pagamento por restauração dental.'),
-(8, 600.00, '2024-06-13', 'Pagamento por tratamento de gengivite.'),
-(9, 200.00, '2024-06-14', 'Pagamento por limpeza dentária.'),
-(10, 800.00, '2024-06-14', 'Pagamento por canal radicular.');
+(1, 100.00, '2024-06-12', 'Consulta de rotina'),
+(2, 150.00, '2024-06-12', 'Canal Radicular'),
+(3, 200.00, '2024-06-12', 'Extração de Dente'),
+(4, 250.00, '2024-06-12', 'Consulta de rotina'),
+(5, 300.00, '2024-06-12', 'Implante Dentário'),
+(6, 350.00, '2024-06-13', 'Prótese Dentária'),
+(7, 400.00, '2024-06-13', 'Restauração Dental'),
+(8, 450.00, '2024-06-13', 'Tratamento de Gengivite'),
+(9, 500.00, '2024-06-14', 'Consulta de rotina'),
+(10, 550.00, '2024-06-14', 'Canal Radicular');
 GO
