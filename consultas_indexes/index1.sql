f20874fa6c1aeeedfceebfadeb8ce4f3a441c431
Alexandre Destro <aalexandredestro@gmail.com>
2024-06-24 01:16:55 -0300

    Update index1.sql

--- consultas_indexes/index1.sql
+++ consultas_indexes/index1.sql
@@ -1,1 +1,9 @@
-CREATE CLUSTERED INDEX IX_Dentistas_DentistaID ON Dentistas(DentistaID);
+-- Consultas
+CREATE NONCLUSTERED INDEX IX_Consultas_DataHora ON Consultas (DataHora);
+CREATE NONCLUSTERED INDEX IX_Consultas_PacienteID_DentistaID ON Consultas (PacienteID, DentistaID);
+
+-- Prontuarios
+CREATE NONCLUSTERED INDEX IX_Prontuarios_PacienteID_DentistaID_Data ON Prontuarios (PacienteID, DentistaID, Data);
+
+-- Receitas
+CREATE NONCLUSTERED INDEX IX_Receitas_PacienteID_DataReceita ON Receitas (PacienteID, DataReceita);
